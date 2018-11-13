b2d99da195c7d0b53d074a0f604adc109303050d
DATAUNIRIO <duttross@gmail.com>
2018-11-13 15:22:32 -0200

    boas vindas

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("para o pacote funcionar, organize uma pasta contendo: \n 1. imagens do seu template \n 2.arquivo Rmarkdown \n 3. lista de presença")
+}
